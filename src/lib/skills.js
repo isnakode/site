c82449e908c1c0e12f27c435e21c872850a373e8
isnakode <isnainisasaki@gmail.com>
2024-11-07 12:16:17 +0800

    first commit

---
+++ src/lib/skills.js
@@ -0,0 +1,38 @@
+export const langs = [
+  { title: 'HTML', icon: '/icons/html.svg' },
+  { title: 'CSS', icon: '/icons/css.svg' },
+  { title: 'PHP', icon: '/icons/php.svg' },
+  { title: 'javascript', icon: '/icons/javascript.svg' },
+  { title: 'typescript', icon: '/icons/typescript.svg' },
+  { title: 'dart', icon: '/icons/dart.svg' },
+  { title: 'python', icon: '/icons/python.svg' },
+]
+export const tools = [
+  { title: 'bun', icon: '/icons/bun.svg' },
+  { title: 'NPM', icon: '/icons/npm.svg' },
+  { title: 'git', icon: '/icons/git.svg' },
+  { title: 'PNPM', icon: '/icons/pnpm.svg' },
+  { title: 'composer', icon: '/icons/composer.svg' },
+]
+export const fws = [
+  { title: 'fastapi', icon: '/icons/fastapi.svg' },
+  { title: 'Drizzle ORM', icon: '/icons/drizzle.jpg' },
+  { title: 'prisma', icon: '/icons/prisma.svg' },
+  { title: 'svelte', icon: '/icons/svelte.svg' },
+  { title: 'vue', icon: '/icons/vue.svg' },
+  { title: 'hono', icon: '/icons/hono.svg' },
+  { title: 'flutter', icon: '/icons/flutter.svg' },
+  { title: 'laravel', icon: '/icons/laravel.svg' },
+  { title: 'code igniter', icon: '/icons/codeigniter.svg' },
+]
+
+export const apps = [
+  { title: 'Word1', icon: '/icons/icon_word1.png' },
+  { title: 'Animal word', icon: '/icons/icon_animal_word.webp' },
+  { title: 'Zikry', icon: '/icons/icon_zikry.webp' },
+]
+export const sosmed = [
+  { link: 'https://github.com/isnakode', icon: '/icons/github.svg' },
+  { link: 'https://play.google.com/store/apps/developer?id=isnakode', icon: '/icons/playstore.svg' },
+  { link: 'https://chromewebstore.google.com/search/isnakode', icon: '/icons/chrome.svg' },
+]
---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+  import { apps, fws, langs, sosmed, tools } from "$lib/skills";
+</script>
+
+<main class="p-8 flex flex-col items-center text-center">
+  <h1 class="text-3xl font-semibold mb-8">IsnaKode</h1>
+  <p>
+    Kumpulan
+    <span class="bg-red-400 text-white p-1.5 pt-0.5 rounded-md">
+      Portfolio
+    </span>
+    oleh
+    <span class="bg-blue-500 text-white p-1.5 pt-0.5 rounded-md">
+      Muhammad isnaini
+    </span>
+  </p>
+  <h2 class="text-2xl font-medium my-8">Sosial media</h2>
+  <div class="flex flex-wrap justify-center gap-8">
+    {#each sosmed as { link, icon }}
+      <div class="size-8 hover:scale-125 transition-all">
+        <a href={link} target="_blank">
+          <img src={icon} class="size-full" alt="sosial media" /></a
+        >
+      </div>
+    {/each}
+  </div>
+  <h2 class="text-2xl font-medium my-8">Aplikasi dan tool yang dibuat</h2>
+  <div class="flex flex-wrap justify-center gap-8">
+    {#each apps as { title, icon }}
+      <div
+        class="size-16 md:size-24 rounded-xl overflow-hidden hover:scale-125 transition-all"
+      >
+        <img src={icon} alt={title} />
+      </div>
+    {/each}
+  </div>
+  <h2 class="text-2xl font-medium my-8">Bahasa pemrograman</h2>
+  <div class="flex flex-wrap justify-center gap-8">
+    {#each langs as { title, icon }}
+      <div
+        class="size-16 md:size-24 rounded-xl overflow-hidden hover:scale-125 transition-all"
+      >
+        <img src={icon} alt={title} />
+      </div>
+    {/each}
+  </div>
+  <h2 class="text-2xl font-medium my-8">Framework</h2>
+  <div class="flex flex-wrap justify-center gap-8">
+    {#each fws as { title, icon }}
+      <div
+        class="size-16 md:size-24 rounded-xl overflow-hidden hover:scale-125 transition-all"
+      >
+        <img src={icon} alt={title} class="size-full" />
+      </div>
+    {/each}
+  </div>
+  <h2 class="text-2xl font-medium my-8">Tools</h2>
+  <div class="flex flex-wrap justify-center gap-8">
+    {#each tools as { title, icon }}
+      <div
+        class="size-16 md:size-24 rounded-xl overflow-hidden hover:scale-125 transition-all"
+      >
+        <img src={icon} alt={title} class="size-full" />
+      </div>
+    {/each}
+  </div>
+</main>
